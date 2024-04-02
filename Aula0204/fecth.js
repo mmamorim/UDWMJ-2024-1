eeb3e88517eaf286e4f0863140d68737c1e5b2f1
mmamorim <marc.m.amorim@gmail.com>
2024-04-02 11:48:27 -0300

    v7

--- Aula0204/fecth.js
+++ Aula0204/fecth.js
@@ -1,12 +1,28 @@
 console.log("HELLO fecth!");
 
+let btn = document.getElementById("btn01")
+console.log("btn",btn);
 
-let url = "https://randomuser.me/api/"
-
-fetch(url).then((resp) => {
-    resp.json().then((obj) => {
-        let pessoa = obj.results[0]
-        console.log("chegou",pessoa);
-        console.log("pessoa.name",pessoa.name);
+btn.onclick = function() {
+    let elem = document.getElementById("boxName")
+    elem.innerText = "Carregando..."
+    let url = "https://randomuser.me/api/"
+    fetch(url).then((resp) => {
+        resp.json().then((obj) => {
+            let user = obj.results[0]
+            console.log("chegou");
+            userRender(user)
+        })
     })
-})
+}
+
+function userRender(user) {
+    console.log("user",user);
+    let nome = user.name.title + " " + user.name.first + " " + user.name.last
+    console.log("nome",nome);
+    let elem = document.getElementById("boxName")
+    elem.innerText = nome
+    let photo = user.picture.large
+    elem = document.getElementById("photo")
+    elem.src = photo
+}
