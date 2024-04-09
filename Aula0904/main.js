456d071dedbe4c054eed9466e3b010737237f7c5
mmamorim <marc.m.amorim@gmail.com>
2024-04-09 10:43:07 -0300

    v9

--- Aula0904/main.js
+++ Aula0904/main.js
@@ -1,4 +1,9 @@
 import todolist from "./todolist.js";
 
-console.log("oi todolist",todolist);
+//console.log("oi todolist",todolist);
 
+//todolist.add("Ir ao cinema",false)
+//todolist.add("Pagar conta",false)
+//todolist.add("Estudar Javascript",false)
+
+console.log("oi todolist",todolist);
--- Aula0904/todolist.js
+++ Aula0904/todolist.js
@@ -1,6 +1,25 @@
 
 const todolist = {
+    tasks: [],
 
+    init() {
+        let objtext = localStorage.getItem("myTasks")
+        if(objtext != null) {
+            this.tasks = JSON.parse(objtext)
+        }
+    },
+
+    add(textDescription, done) {
+        let obj = {
+            textDescription,
+            done
+        }
+        this.tasks.push(obj)
+        let objtext = JSON.stringify(this.tasks)
+        localStorage.setItem("myTasks",objtext)
+    }
 }
+
+todolist.init()
 
 export default todolist
